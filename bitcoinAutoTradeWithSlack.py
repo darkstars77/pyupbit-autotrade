c7b071350efdab277dd0300a81e3e1d898e4db3d
darkstars77 <darkstars77@naver.com>
2021-05-23 23:33:33 +0900

    update my setting(bitcoin Auto trading)

--- bitcoinAutoTradeWithSlack.py
+++ bitcoinAutoTradeWithSlack.py
@@ -2,10 +2,15 @@ import time
 import pyupbit
 import datetime
 import requests
+import json
 
-access = "your-access"
-secret = "your-secret"
-myToken = "xoxb-your-token"
+with open('../pw.json') as json_file:
+    json_data = json.load(json_file)
+
+
+access = json_data['access']
+secret = json_data['secret']
+myToken = json_data['myToken']
 
 def post_message(token, channel, text):
     """슬랙 메시지 전송"""
@@ -26,11 +31,11 @@ def get_start_time(ticker):
     start_time = df.index[0]
     return start_time
 
-def get_ma15(ticker):
-    """15일 이동 평균선 조회"""
-    df = pyupbit.get_ohlcv(ticker, interval="day", count=15)
-    ma15 = df['close'].rolling(15).mean().iloc[-1]
-    return ma15
+def get_ma(ticker, d):
+    """d일 이동 평균선 조회"""
+    df = pyupbit.get_ohlcv(ticker, interval="day", count=d)
+    ma = df['close'].rolling(d).mean().iloc[-1]
+    return ma
 
 def get_balance(ticker):
     """잔고 조회"""
@@ -50,29 +55,35 @@ def get_current_price(ticker):
 # 로그인
 upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")
+
+# parameter
+my_ticker='KRW-BTC'
+ma_value=5 # moving average value
+k=0.5 #변동성 계수 통상적으로 0.5 사용
+
 # 시작 메세지 슬랙 전송
-post_message(myToken,"#crypto", "autotrade start")
+post_message(myToken,"#crypto", "{} autotrade start".format(my_ticker))
 
 while True:
     try:
         now = datetime.datetime.now()
-        start_time = get_start_time("KRW-BTC")
+        start_time = get_start_time(my_ticker)
         end_time = start_time + datetime.timedelta(days=1)
 
         if start_time < now < end_time - datetime.timedelta(seconds=10):
-            target_price = get_target_price("KRW-BTC", 0.5)
-            ma15 = get_ma15("KRW-BTC")
-            current_price = get_current_price("KRW-BTC")
-            if target_price < current_price and ma15 < current_price:
+            target_price = get_target_price(my_ticker, k)
+            ma = get_ma(my_ticker, ma_value)
+            current_price = get_current_price(my_ticker)
+            if target_price < current_price and ma < current_price:
                 krw = get_balance("KRW")
                 if krw > 5000:
-                    buy_result = upbit.buy_market_order("KRW-BTC", krw*0.9995)
-                    post_message(myToken,"#crypto", "BTC buy : " +str(buy_result))
+                    buy_result = upbit.buy_market_order(my_ticker, krw*0.9995)
+                    post_message(myToken,"#crypto", "{} buy : ".format(my_ticker) +str(buy_result))
         else:
-            btc = get_balance("BTC")
+            btc = get_balance(my_ticker.split('-')[-1]) #my_ticker의 A-B -> B 의미
             if btc > 0.00008:
-                sell_result = upbit.sell_market_order("KRW-BTC", btc*0.9995)
-                post_message(myToken,"#crypto", "BTC buy : " +str(sell_result))
+                sell_result = upbit.sell_market_order(my_ticker, btc*0.9995)
+                post_message(myToken,"#crypto", "{} sell : ".format(my_ticker) +str(sell_result))
         time.sleep(1)
     except Exception as e:
         print(e)
